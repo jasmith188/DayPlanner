4d9fe353cb75ddc0239c285f0cda88dcb6107faa
Jimmy Smith <jasmith.188@gmail.com>
2020-03-20 15:45:19 -0400

    adding notes

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
     //global array
-    var K = ["nIne", "tEn", "eLeven", "tWelve", "oNe", "tWo", "tHree", "fOur", "fIve"];
+    var K = ["nine", "ten", "eleven", "twelve", "one", "two", "three", "four", "five"];
     var Area = ["#text9", "#text10", "#text11", "#text12", "#text1", "#text2", "#text3", "#text4", "#text5"];
     console.log(hour);
     
     //SEtting Time
+    setInterval(function() {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-
-    //setInterval(moment(), 1000),
+    }, 1000)
+    
+    
 
     //variable to help retrieve time in dom
-    var today = new Date();
-    var hour = today.getHours();
-
-    for (var i = 0; i < array.length; index++) {
-
+        var today = new Date();
+        var hour = today.getHours();
+    //use yhr loop to go over all the text areas on tht html
+    for (var i = 0; i < Area.length; i++) {
+        //set a data time into  avarabe
         var data = $(Area[i]).attr("data-time");
+      //change the string to a number to use in in an if statement  
         data = parseInt(data);
-
+        //future if
         if (hour < data) {
             $(Area[i]).addClass("col-md-10 future description")
         }
+        //present if 
         if (hour === data) {
             $(Area[i]).addClass("col-md-10 present description")
         }
+        // past if
         if (hour > data) {
             $(Area[i]).addClass("col-md-10 past description")
         }
@@ -46,15 +51,12 @@ $(document).ready(function () {
         //cool gets all the sets in local storage
         console.log(localStorage.getItem(K[i]));
         //if local storage is null then loop will skip
+        var cool = localStorage.getItem(K[i]);
         if (cool != null) {
             $(Area[i]).text(cool);
         }
 
     }
-
-
-
-
-    console.log(localStoage.length);
+    
 
 })
