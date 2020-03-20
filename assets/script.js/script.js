c0186ccb6554ac4d3b3f3e14af5efe6e8540d2e8
Jimmy Smith <jasmith.188@gmail.com>
2020-03-19 22:45:37 -0400

    colors

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
+    //global array
+    var K = ["nIne", "tEn", "eLeven", "tWelve", "oNe", "tWo", "tHree", "fOur", "fIve"];
+    var Area = ["#text9", "#text10", "#text11", "#text12", "#text1", "#text2", "#text3", "#text4", "#text5"];
 
+    //SEtting Time
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-//SEtting Time
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+    //setInterval(moment(), 1000),
 
-//setInterval(moment(), 1000),
+    //variable to help retrieve time in dom
+    var today = new Date();
+    var hour = today.getHours();
 
-//Adding text to local Storage
-//localStorage.setItem()
+    for (var i = 0; i < array.length; index++) {
 
-//var hour = today.get(Hours)))
-//if html < ()
-//addClass ("blue")
+        var data = $(Area[i]).attr("data-time");
+        console.log(data);
 
+        if (hour < data) {
+            $(Area[i]).addClass("col-md-10 future description")
+        }
+        if (hour < data) {
+            $(Area[i]).addClass("col-md-10 present description")
+        }
+        if (hour < data) {
+            $(Area[i]).addClass("col-md-10 past description")
+        }
 
-
-
-
-//adding n event listener to text content
+    }
+    //adding n event listener to text content
 
     $(".saveBtn").on("click", function () {
         //get nearby values using sibling method:
@@ -27,19 +38,22 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
         localStorage.setItem(time, value);
     })
 
-var K = ["nIne", "tEn" , "eLeven", "tWelve", "oNe", "tWo", "tHree", "fOur", "fIve"];
-var Area = ["#text9", "#text10", "#text11", "#text12", "#text1", "#text2", "#text3", "#text4", "#text5"];
-//$("#text9").textLocalStorage.getIem("nIne"));
+    //$("#text9").textLocalStorage.getIem("nIne"));
+
+    //for loop will match the arraysabove and link them via key method and then place them back on the screen.
+    for (let i = 0; i < K.length; i++) {
+        //cool gets all the sets in local storage
+        console.log(localStorage.getItem(K[i]));
+        //if local storage is null then loop will skip
+        if (cool != null) {
+            $(Area[i]).text(cool);
+        }
+
+    }
 
 
 
 
-
-
-
-
-
-
-
+    console.log(localStoage.length);
 
 })
